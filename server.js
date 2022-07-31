31bae57b13dd2641dd0158cab855afbd9e247fa9
ak-2002 <awkiiru@usiu.ac.ke>
2022-07-31 20:06:03 +0300

    adding server section

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+const cors = require('cors');
+
+
+
+app.use(cors({
+    origin: 'http://localhost:3000'
+}));
+
+var server = app.listen(8081, function () {
+   var host = server.address().address;
+   var port = server.address().port
+   
+   console.log("Example app listening at http://%s:%s", host, port)
+})
