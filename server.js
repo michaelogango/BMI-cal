b7601d592aa0aa7425c89d9eda2795f641750f0f
Michael Odhiambo <michaelodhiambo@Michaels-MacBook-Air.local>
2022-07-31 23:15:53 +0300

    make it better

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 const cors = require('cors');
 
+const port=process.env.PORT||3002
+
 app.get('/', function (req, res) {
     console.log("Got a GET request for the homepage");
     res.send('back-end up and ready');
@@ -11,9 +13,11 @@ app.use(cors({
     origin: 'http://localhost:3000'
 }));
 
-var server = app.listen(8081, function () {
+var server = app.listen(3002, function () {
    var host = server.address().address;
    var port = server.address().port
+
+   console.log(`server listening to port${port}`)
    
    console.log("Example app listening at http://%s:%s", host, port)
 })
